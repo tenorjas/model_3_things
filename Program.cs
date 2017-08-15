59ecc6ca98af74f73331bc32cdf7bdbebc117b97
Jonathan Schultz <jonathan.a.schultz@gmail.com>
2017-08-15 09:44:45 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace model_3_things
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
